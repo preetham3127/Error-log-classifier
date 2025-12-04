59f9e9a75015cc86ca3e3f2ef8b3bb5ce8f4f587
ffpreetham296@gmail.com <ffpreetham296@gmail.com>
2025-12-04 20:08:35 +0530

    chore(src): add commit marker 7

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""
+Error Log Classifier - Package
+"""
+
+__version__ = "1.0.0"
+__author__ = "Group 106 - Preetham Ghorpade & Harish R S"
+__description__ = "Error log analysis and clustering tool with CSV and HTML exports"
+
+
+# commit_marker_7
+_commit_marker_7 = 7
+
